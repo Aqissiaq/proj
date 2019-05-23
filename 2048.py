6b6422aa7a32286877b6fd0cdf369aeebbd81f22
kadul <kaduliho@live.no>
2019-05-23 14:34:18 +0200

    more changes

--- 2048.py
+++ 2048.py
@@ -37,17 +37,17 @@ I wanted to try to write a version of the game myself, that code
 
 class Game():
 
-    def __init__(self, size=4, MSet="wasd", cont=True, win=False, points=0, winNumb=2048):
+    def __init__(self, size=4, MSet="wasd", cont=True, points=0, winNumb=2048):
         self.size = size
         self.field = np.zeros((self.size, self.size), dtype=int)
         self.mset = MSet
         self.cont = cont
-        self.win = win
         self.points = points
-        self.winNumb = Game.validateWinNumb(winNumb)
 
-        self.moveDict = Game.makeMoveDict(self.mset)
         self.currentMove = None
+
+        self.winNumb = Game.validateWinNumb(winNumb)
+        self.moveDict = Game.makeMoveDict(self.mset)
 
         # self.formatter = {
         #     int:
@@ -55,7 +55,7 @@ class Game():
 
     @staticmethod
     def validateWinNumb(n):
-        if bool(n and not (n&(n-1))):
+        if bool(n and not (n & (n - 1))):
             return n
         print("Your target number cannot be reached in this game")
         exit(0)
@@ -89,23 +89,21 @@ class Game():
             self.cont = False
             return self
         ind = ra.randrange(tempX.shape[0])
-        self.field[tempX[ind]][tempY[ind]] = int(np.random.choice((2, 4), 1, p=(.8, .2)))
-        self.points += self.field[tempX[ind]][tempY[ind]]
+        newVal = int(np.random.choice((2, 4), 1, p=(.8, .2)))
+        self.field[tempX[ind]][tempY[ind]] = newVal
+        self.points += newVal
         return self
 
 
     def executeMove(self):
 
         def getMove():
-            move = input('\n'+ self.mset + "?\n>>> ")
+            move = input('\n' + self.mset + "?\n>>> ")
+            if move == 'esc':    exit(0)
+
             if len(move) != 1 or move not in self.mset:
                 move = getMove()
             return move
-
-        def handleRow(line, n):
-            temp_row = np.delete(line, np.where(line == 0))
-            newRow = self.mergeLine(temp_row) if len (temp_row) > 1 else temp_row
-            return np.append(newRow, [0 for k in range((n - len(newRow)))])
 
         self.currentMove = getMove()
 
@@ -114,59 +112,106 @@ class Game():
 
         for i in range(self.size):
             # print(f"row{i}", temp_field[i])
-            temp_field[i] = handleRow(temp_field[i], self.size)
+            temp_field[i] = self.handleRow(temp_field[i], self.size)
 
         self.field = np.rot90(temp_field, rotKey[1])
         self.currentMove = None
         return self
 
 
+    def handleRow(self, row, n):
+        temp_row = np.delete(row, np.where(row == 0))
+        newRow = self.mergeLine(temp_row) if len(temp_row) > 1 else temp_row
+        return np.append(newRow, [0 for k in range((n - len(newRow)))])
+
+
     def mergeLine(self, line):
-        # print("line", line, sep='\t', end='\n\n')
-        temp = line.tolist()    # for changing values inline
-        rtn = []                # Gather the values to be returned
+        temp = line.tolist()  # for changing values inline
+        rtn = []  # Gather the values to be returned
 
         for j in range(len(temp) - 1):
             if (temp[j] == temp[j + 1]):
-                sc = line[j] + line[j+1]
+                sc = line[j] + line[j + 1]
                 self.points += (sc)
-                temp[j] = sc
-                temp[j+1] = 0
+                temp[j] = sc;
+                temp[j + 1] = 0
 
-            if temp[j] != 0:
-                rtn.append(temp[j])
-
+            if temp[j] != 0:    rtn.append(temp[j])
         # Try to add the last elemet in the list
-        if temp[-1] != 0:
-            rtn.append(temp[-1])
+        if temp[-1] != 0:   rtn.append(temp[-1])
 
         return np.asarray(rtn, dtype=int)
 
 
-    def CheckWinAndContinue(self):
-        if self.winNumb in self.field.flatten():
-            self.win = True
-            print("You have won!")
-            c = input("Type 'yes' if you wish to continue playing\n>>> ").lower()
-            self.cont = True if c == 'yes' else False
-        # return not self.win or self.cont
+    def movePossible(self):
+        #TODO: make this methos a part of the game
+        # game does not end when there are no possible moves left
+        a = '''
+
+        def handleRow(row, n):
+            temp_row = np.delete(row, np.where(row == 0))
+            newRow = handelMerge(temp_row) if len(temp_row) > 1 else temp_row
+            return np.append(newRow, [0 for k in range((n - len(newRow)))])
+
+        def handelMerge(line):
+            temp = line.tolist();
+            rtn = []
+
+            for j in range(len(temp) - 1):
+                if (temp[j] == temp[j + 1]):
+                    temp[j] = line[j] + line[j + 1]
+                    temp[j + 1] = 0
+                if temp[j] != 0:    rtn.append(temp[j])
+
+            if temp[-1] != 0:   rtn.append(temp[-1])
+            return np.asarray(rtn, dtype=int)
+
+        nowField = self.field.copy()
+        print("NOW\n", nowField)
+
+        for rotKey in self.moveDict.values():
+            print(rotKey)
+            temp_field = np.rot90(nowField, rotKey[0])
+
+            for i in range(self.size):
+                # print(f"row{i}", temp_field[i])
+                temp_field[i] = handleRow(temp_field[i], self.size)
+
+            testField = np.rot90(temp_field, rotKey[1])
+            print("TEST\n", testField)
+
+            if np.equal(nowField.all(), testField.all()):
+                return False
+        return True
+        '''
         return self
 
-
     def play(self, n=20):
-        # FIXME: while loop not terminating right as it should
+        print("Welcome to this game of 2048\n"
+              f"Move the tiles till you reach {self.winNumb}!")
 
         self.add2or4()
-        while (not self.win or self.cont):
-            self.CheckWinAndContinue()
+        while (self.cont):
+
+            if self.winNumb in self.field.flatten():
+                self.printField()
+                print("Congratulations!")
+                c = input("Type 'yes' if you wish to continue playing\n>>> ").lower()
+                if 'yes' in c:
+                    self.winNumb *= 2
+                    print(f'Your new target is {self.winNumb}!\n')
+                else:
+                    break
+
             self.add2or4().printField().executeMove()
+        self.printField()
 
 
 def run():
     game = Game(size=3, MSet='wasd', winNumb=16)
-    game.play()  # While loop is in this method - when loop ends, game is over
-    game.printField()
-    # print(f"You got {game.points} points!")
+    game.play()
+    # Game().play()
+    print("\nGame is over, thank you for playing")
 
 
 run()
